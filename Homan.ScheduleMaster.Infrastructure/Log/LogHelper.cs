2fadf351f1ecdabd32aaa48277112f8271278bae
heao <591310381@qq.com>
2018-05-29 18:09:18 +0800

    'log'

---
+++ Homan.ScheduleMaster.Infrastructure/Log/LogHelper.cs
@@ -0,0 +1,99 @@
+﻿using Homan.ScheduleMaster.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homan.ScheduleMaster.Infrastructure.Log
+{
+    public class LogHelper
+    {
+
+        public static void Info(string message)
+        {
+            LogManager.Queue.Write(new SystemLog
+            {
+                Category = (int)LogCategory.Info,
+                Contents = message,
+                CreateTime = DateTime.Now
+            });
+        }
+
+        public static void Info(string message, Guid task)
+        {
+            LogManager.Queue.Write(new SystemLog
+            {
+                Category = (int)LogCategory.Info,
+                Contents = message,
+                TaskId = task,
+                CreateTime = DateTime.Now
+            });
+        }
+
+        public static void Warn(string message)
+        {
+            LogManager.Queue.Write(new SystemLog
+            {
+                Category = (int)LogCategory.Warn,
+                Contents = message,
+                CreateTime = DateTime.Now
+            });
+        }
+
+        public static void Warn(string message, Guid task)
+        {
+            LogManager.Queue.Write(new SystemLog
+            {
+                Category = (int)LogCategory.Warn,
+                Contents = message,
+                TaskId = task,
+                CreateTime = DateTime.Now
+            });
+        }
+
+        public static void Error(string message)
+        {
+            LogManager.Queue.Write(new SystemLog
+            {
+                Category = (int)LogCategory.Error,
+                Contents = message,
+                CreateTime = DateTime.Now
+            });
+        }
+
+        public static void Error(string message, Guid task)
+        {
+            LogManager.Queue.Write(new SystemLog
+            {
+                Category = (int)LogCategory.Error,
+                Contents = message,
+                TaskId = task,
+                CreateTime = DateTime.Now
+            });
+        }
+
+        public static void Error(string message, Exception exp)
+        {
+            LogManager.Queue.Write(new SystemLog
+            {
+                Category = (int)LogCategory.Error,
+                Contents = $"##{message}##  {exp.Message}",
+                TaskId = Guid.Empty,
+                CreateTime = DateTime.Now
+            });
+        }
+
+        public static void Error(string message, Exception exp, Guid task)
+        {
+            LogManager.Queue.Write(new SystemLog
+            {
+                Category = (int)LogCategory.Error,
+                Contents = $"##{message}##  {exp.Message}",
+                TaskId = task,
+                CreateTime = DateTime.Now
+            });
+        }
+    }
+
+}
---
+++ Homan.ScheduleMaster.Infrastructure/Log/LogManager.cs
@@ -0,0 +1,44 @@
+﻿
+using Homan.ScheduleMaster.Core;
+using Homan.ScheduleMaster.Core.Models;
+using Homan.ScheduleMaster.Infrastructure.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homan.ScheduleMaster.Infrastructure.Log
+{
+    public class LogManager
+    {
+        public static BufferQueue<SystemLog> Queue;
+
+        public static void Init()
+        {
+            Queue = new BufferQueue<SystemLog>();
+            var td = new System.Threading.Thread(() =>
+            {
+                using (TaskDbContext db = new TaskDbContext())
+                {
+                    while (true)
+                    {
+                        Queue.Read((item, index) =>
+                        {
+                            db.SystemLog.Add(item);
+                        });
+                        db.SaveChanges();
+                        System.Threading.Thread.Sleep(5000);
+                    }
+                }
+            });
+            td.IsBackground = true;
+            td.Start();
+        }
+
+        public static void Shutdown()
+        {
+            Queue.Clear();
+        }
+    }
+}
